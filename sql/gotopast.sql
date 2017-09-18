6bf4de0f4d4538bcffc3fa8f9819d7eec5e4b287
tangocho01 <ctomasi4sg@gmail.com>
2017-09-18 17:42:05 +0900

    go back to 1 day not 7

--- sql/gotopast.sql
+++ sql/gotopast.sql
@@ -1,2 +1,2 @@
-update cards set last_time =  current_timestamp - ( interval '7 day');
+update cards set last_time =  current_timestamp - ( interval '1 day');
 update decks set last_access =  current_timestamp - ( interval '1 day');
